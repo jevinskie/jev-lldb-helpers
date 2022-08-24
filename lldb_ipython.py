22f15e1cf4d4f9c3153b15ee978e1f575b55a4ff
Jevin Sweval <jevinsweval@gmail.com>
2022-08-24 19:09:29 -0400

    add black/isort pre-commit

--- lldb_ipython.py
+++ lldb_ipython.py
@@ -2,25 +2,25 @@ import sys
 
 if sys.version_info.major >= 3:
 
-    import lldb
     # import commands
     import optparse
     import shlex
 
     import IPython
+    import lldb
     from traitlets.config import Config
 
     # from rich import print as rprint
     # from rich import inspect as rinspect
 
     def __lldb_init_module(debugger, internal_dict):
-        debugger.HandleCommand('command script add -f lldb_ipython.ipython ipython')
+        debugger.HandleCommand("command script add -f lldb_ipython.ipython ipython")
         # print('dump_mem command added')
 
     def create_ipython_options():
         usage = "usage: %prog [options]"
-        description='''IPython woohoo'''  
-        parser = optparse.OptionParser(description=description, prog='ipython',usage=usage)
+        description = """IPython woohoo"""
+        parser = optparse.OptionParser(description=description, prog="ipython", usage=usage)
         # parser.add_option('-a', '--address', type='int', dest='address', help='Address to dump.')
         # parser.add_option('-s', '--size', type='int', dest='size', help='Size of dump.')
         # parser.add_option('-f', '--file', type='string', dest='path', help='File to dump into.')
@@ -37,7 +37,7 @@ if sys.version_info.major >= 3:
 
         except:
             # if you don't handle exceptions, passing an incorrect argument to the OptionParser will cause LLDB to exit
-            result.SetError ("option parsing failed")
+            result.SetError("option parsing failed")
             return
         # print(options)
 
@@ -50,17 +50,16 @@ if sys.version_info.major >= 3:
 
         c = Config()
         c.InteractiveShellApp.exec_lines = [
-            'lldb.target = lldb.debugger.GetSelectedTarget()',
-            'lldb.process = lldb.target.GetProcess()',
-            'lldb.thread = lldb.process.GetSelectedThread()',
-            'lldb.frame = lldb.thread.GetSelectedFrame()',
+            "lldb.target = lldb.debugger.GetSelectedTarget()",
+            "lldb.process = lldb.target.GetProcess()",
+            "lldb.thread = lldb.process.GetSelectedThread()",
+            "lldb.frame = lldb.thread.GetSelectedFrame()",
         ]
         # c.InteractiveShell.colors = 'LightBG'
-        c.InteractiveShell.colors = 'NoColor'
+        c.InteractiveShell.colors = "NoColor"
         c.InteractiveShell.confirm_exit = False
         c.TerminalIPythonApp.display_banner = False
         # IPython.terminal.embed.embed(config=c)
         IPython.start_ipython(user_ns=internal_dict, config=c)
 
         return
-
